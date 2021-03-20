551a2eec333c6bd6642ed5256212510246b36579
31 <331300+31@users.noreply.github.com>
2021-03-20 09:17:56 -0500

    Add base._Ready() to fix inherited OnReadyGet

--- godot/SpawnButton.cs
+++ godot/SpawnButton.cs
@@ -5,7 +5,7 @@ public partial class SpawnButton : Button
 {
 	[OnReadyGet("res://Subgui.tscn", Private = true)] public PackedScene _scene;
 
-	public void OnPress()
+	public virtual void OnPress()
 	{
 		GetParent().AddChild(_scene.Instance());
 	}
