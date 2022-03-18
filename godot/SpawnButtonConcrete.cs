8d4ae1841a9f2a6f4c28f0bb956c26e063767074
31 <331300+31@users.noreply.github.com>
2022-03-17 23:50:35 -0500

    Fix generic partial generation (#31)
    Add a class that exercises this behavior to the dev project.
    
    Let SDK version roll forward in global.json, so I can work on the project again after some .NET versions have gone by.
    
    Make the dev project always emit the generated files, for convenience.

---
+++ godot/SpawnButtonConcrete.cs
@@ -0,0 +1,1 @@
+﻿public class SpawnButtonConcrete : SpawnButtonGeneric<int> { }
---
+++ godot/SpawnButtonGeneric.cs
@@ -0,0 +1,9 @@
+﻿using GodotOnReady.Attributes;
+
+public partial class SpawnButtonGeneric<T> : SpawnButton
+{
+	[OnReady] private void SetupText()
+	{
+		Text = $"My type is {typeof(T)}!";
+	}
+}
